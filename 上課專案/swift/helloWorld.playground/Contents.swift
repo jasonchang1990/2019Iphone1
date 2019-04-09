fe2d1cc6afd0370c65d6fc465cb5eb15c6c874b1
徐國堂 <roberthus2003@gmail.com>
2019-04-09 21:30:07 +0800

    Update Contents.swift

--- 上課專案/swift/helloWorld.playground/Contents.swift
+++ 上課專案/swift/helloWorld.playground/Contents.swift
@@ -23,3 +23,26 @@ let appleSummary = "I have " + String(apples) + " apples  and " + String(oranges
 //字串插補
 let appleSummary1 = "I have \(apples) apples and \(oranges) oranges"
 let fruitSummary = "I have \(apples + oranges)pieces of fruit"
+
+
+let quotation = """
+《美國之音》報導，其實數年前中國就曾流傳，反匿蹤雷達發現並鎖定F-22匿蹤戰
+機的說法，不過先前都被認為只是一種宣傳手法。但去年12月24日
+，匿蹤雷達偵測專家吳劍旗在一次演講中證實，中國使用米波雷
+達早就可對F-22進行遠距跟蹤，雖然過往有低仰角盲區大、精度達不到引導火控雷達，以及戰機對來襲目標進行攔截等缺陷
+"""
+print(quotation)
+
+var shoppingList = ["catfish", "water", "tulips"];
+shoppingList[0] = "bottle of water"
+shoppingList[0]
+
+var occupations = [
+    "Malcolm" : "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Malcolm"]
+occupations["Kaylee"]
+occupations["kaylee"]
+
