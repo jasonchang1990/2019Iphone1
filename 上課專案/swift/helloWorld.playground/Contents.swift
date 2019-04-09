e2592eb6b3546b1fb123f0ce3346c819c5a20835
徐國堂 <roberthus2003@gmail.com>
2019-04-09 21:01:47 +0800

    Update Contents.swift

--- 上課專案/swift/helloWorld.playground/Contents.swift
+++ 上課專案/swift/helloWorld.playground/Contents.swift
@@ -9,3 +9,17 @@ let myConstant = 50
 let implictInteger = 70
 let implictDouble = 70.0
 let explicitDouble:Double = 70
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+
+let apples = 3;
+let oranges = 5;
+
+let appleSummary = "I have " + String(apples) + " apples  and " + String(oranges) + " oranges"
+
+//字串插補
+let appleSummary1 = "I have \(apples) apples and \(oranges) oranges"
+let fruitSummary = "I have \(apples + oranges)pieces of fruit"
