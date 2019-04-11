969c8da402b118118cd2dc3abcc123461d7465a5
徐國堂 <roberthus2003@gmail.com>
2019-04-11 19:25:40 +0800

    modify

---
+++ 上課專案/swift/lesson2/lesson2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//let connotBeNegative:UInt8 = -1;
+//let tooBitg:Int8 = Int8.max + 1;
+
+let twoThousand:UInt16 = 2_000;
+let one:UInt8 = 1;
+
+let twoThousandAndOne = twoThousand + UInt16(one)
+
