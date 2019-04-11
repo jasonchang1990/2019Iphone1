292183d04e9f5c515764358b7dca9cf11c431476
徐國堂 <roberthus2003@gmail.com>
2019-04-11 20:00:00 +0800

    Update Contents.swift

--- 上課專案/swift/lesson2/lesson2.playground/Contents.swift
+++ 上課專案/swift/lesson2/lesson2.playground/Contents.swift
@@ -8,3 +8,26 @@ let one:UInt8 = 1;
 
 let twoThousandAndOne = twoThousand + UInt16(one)
 
+//整數和浮點數之間的轉型
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine;
+
+//資料類型的小名
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+maxAmplitudeFound = 3;
+
+//boolean
+//Bool
+let orangesAreOrange = true
+let turnipsAreDelicious = true
+
+if turnipsAreDelicious {
+    print("Mmm,tasty turnips!")
+}else{
+    print("Eww, turnips are horrible.");
+}
+
+
+
