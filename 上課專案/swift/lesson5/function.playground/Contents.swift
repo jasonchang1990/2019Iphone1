9554b649e3943ac14e9de8dd87f8f30d2f8297a3
徐國堂 <roberthus2003@gmail.com>
2019-04-23 20:35:42 +0800

    modify

---
+++ 上課專案/swift/lesson5/function.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func greet(person:String) -> String{
+   let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+print(greet(person: "Anna"))
+print(greet(person: "Brian"));
+
+func greetAgain(person:String) -> String{
+    return "Hello again, " + person + "!"
+}
+
+print(greetAgain(person: "Anna"));
