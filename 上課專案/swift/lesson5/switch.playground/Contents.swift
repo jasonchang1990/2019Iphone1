d6b7cc90dd92d06c4ff2487ab1923c50162ec898
徐國堂 <roberthus2003@gmail.com>
2019-04-23 19:42:12 +0800

    Update Contents.swift

--- 上課專案/swift/lesson5/switch.playground/Contents.swift
+++ 上課專案/swift/lesson5/switch.playground/Contents.swift
@@ -14,3 +14,26 @@ switch somePoint {
         print("其它");
 }
 
+//value binding
+
+let anotherPoint = (2, 5)
+switch anotherPoint{
+    case (let x, 0):
+        print("y是0,x是\(x)")
+    case (0, let y):
+        print("x是0,y是\(y)")
+    case let (x,y):
+        print("x是\(x),y是\(y)");
+}
+
+//binding + where 
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint{
+    case let (x, y) where x==y:
+        print("x==y")
+    case let (x, y) where x == -y:
+    print("x==-y")
+    case let (x, y):
+    print("x=\(x),y=\(y)")
+    
+}
