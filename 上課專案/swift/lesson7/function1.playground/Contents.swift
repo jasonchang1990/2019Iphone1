46e39ee863e329a7a185504f89f299a009347e0a
徐國堂 <roberthus2003@gmail.com>
2019-04-30 19:47:19 +0800

    modify

---
+++ 上課專案/swift/lesson7/function1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//default parameter value
+func someFunction(parameterWithoutDefault:Int, parameterWithDefault:Int = 12){
+    
+}
+
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6)
+someFunction(parameterWithoutDefault: 5)
+
+//不限參數的數量
+func arithmeticMean(_ numbers:Double...) -> Double{
+    var total:Double = 0;
+    for number in numbers{
+        total += number;
+    }
+    return total / Double(numbers.count)
+}
+
+arithmeticMean(1,2,3,4,5,6)
+arithmeticMean(1.4,6,8,4.6,7.8)
