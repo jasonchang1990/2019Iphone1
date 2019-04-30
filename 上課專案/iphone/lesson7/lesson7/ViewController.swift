753a4e88a506ac9efd041c35dc48b13d0e60dff0
徐國堂 <roberthus2003@gmail.com>
2019-04-30 19:27:29 +0800

    modify

---
+++ 上課專案/iphone/lesson7/lesson7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson7
+//
+//  Created by Robert on 2019/4/30.
+//  Copyright © 2019 Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue;
+    }
+
+
+}
+
