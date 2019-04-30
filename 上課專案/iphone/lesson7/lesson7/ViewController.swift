8a1831a16401c19dd8ef3eaf8627c25a9bc0c611
徐國堂 <roberthus2003@gmail.com>
2019-04-30 21:38:13 +0800

    modify

--- 上課專案/iphone/lesson7/lesson7/ViewController.swift
+++ 上課專案/iphone/lesson7/lesson7/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.blue;
+        
+        let greenView = UIView(frame: CGRect.init(x: 113, y: 111, width: 132, height: 149))
+        greenView.backgroundColor = UIColor.green;
+        view.addSubview(greenView)
+        
+        let redView = UIView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
+        redView.backgroundColor = UIColor.red
+        
+        greenView.addSubview(redView)
     }
 
 
