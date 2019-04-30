8580c12285c3a99e771c6114018a8157b70bcd52
徐國堂 <roberthus2003@gmail.com>
2019-04-30 20:02:14 +0800

    Update Contents.swift

--- 上課專案/swift/lesson7/function1.playground/Contents.swift
+++ 上課專案/swift/lesson7/function1.playground/Contents.swift
@@ -19,3 +19,16 @@ func arithmeticMean(_ numbers:Double...) -> Double{
 
 arithmeticMean(1,2,3,4,5,6)
 arithmeticMean(1.4,6,8,4.6,7.8)
+
+//inout parameter
+func swapTwoInt(_ a:inout Int,_ b:inout Int){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3;
+var anotherInt = 107;
+swapTwoInt(&someInt, &anotherInt)
+someInt
+anotherInt
