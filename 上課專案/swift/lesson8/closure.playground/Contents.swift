590a07943663987a607ef97ee95a7771d0eb00a3
徐國堂 <roberthus2003@gmail.com>
2019-05-02 20:20:10 +0800

    Update Contents.swift

--- 上課專案/swift/lesson8/closure.playground/Contents.swift
+++ 上課專案/swift/lesson8/closure.playground/Contents.swift
@@ -14,3 +14,22 @@ reverseNames = names.sorted(by: {
     return s1 < s2
 })
 
+//closure 的所有資料類型,可以省略,因為透過推測
+reverseNames = names.sorted(by: {
+    (s1,s2) in
+    return s1 < s2
+})
+
+//如果只有一行,可以省略return
+reverseNames = names.sorted(by: {
+    (s1,s2) in  s1 < s2
+})
+
+//s1,s2也可以省略去定義,$0,$1
+reverseNames = names.sorted(by: { $0 < $1 })
+reverseNames = names.sorted(by: < )
+
+//trailing closure,在參數的最後一個
+reverseNames = names.sorted{
+    $0 < $1
+}
