d79d07f0b1fdbab187f2ba5b9130c2fca13ea601
徐國堂 <roberthus2003@gmail.com>
2019-05-07 19:56:46 +0800

    modify

--- 上課專案/swift/lesson9/enumeration.playground/Contents.swift
+++ 上課專案/swift/lesson9/enumeration.playground/Contents.swift
@@ -11,9 +11,10 @@ enum CompassPoint{
     case west
 }
 //定義一個工廠
-enum Planet{
+enum Planet:Int{
     case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
 }
+print(Planet.earth.rawValue)
 
 //實作一個實體
 var directionToHead = CompassPoint.west
@@ -30,3 +31,16 @@ switch directionToHead {
     case .west:
         print("west");
 }
+
+let button = UIButton(type: .infoLight)
+
+//建立可以被for in 的列舉
+enum Beverage:CaseIterable{
+    case coffee, tea, juice
+}
+print(Beverage.allCases.count)
+
+for beverage in Beverage.allCases{
+    print(beverage)
+}
+
