3538e10e6e3d67439bff80dc64bd310cc2a5d17c
徐國堂 <roberthus2003@gmail.com>
2019-05-14 19:19:52 +0800

    modify

---
+++ 上課專案/swift/lesson11/property.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+//store property
+struct FixedLengthRange{
+    var firstValue:Int
+    let length:Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+
+rangeOfThreeItems.firstValue = 6;
+
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+//rangeOfFourItems.firstValue = 6;
