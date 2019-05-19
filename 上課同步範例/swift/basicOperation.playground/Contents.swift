1e4a17c2a6be3ba63910f10ea6625f69a8154075
徐國堂 <roberthus2003@gmail.com>
2019-05-19 14:47:45 +0800

    modify

--- 上課同步範例/swift/basicOperation.playground/Contents.swift
+++ 上課同步範例/swift/basicOperation.playground/Contents.swift
@@ -15,6 +15,47 @@ let 🐶 = "dog"
 print(🐶)
 
 
+var fridentlyWelcome = "Hello!"
+fridentlyWelcome = "Bonjour!"
+/*
+let languageName = "Swift"
+//languageName = "swift++"
 
+print("The current value of friendlyWelcome is \(fridentlyWelcome)");
+
+// This is a commnet.
+
+*/
+
+let min = UInt8.min
+var maxValue = UInt8.max
+print(Int.min)
+print(Int.max)
+
+let pi = 3.14159
+let anotherPi = 3.0 + pi
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000;
+let justOverOneMillion = 1_000_000.000_000_1
+
+let twoThousand:UInt16 = 2_000
+let one:UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one);
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+
+let pi1 = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi)
+
+//資料類型的小名
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+//Boolean true,false
+let orangesAreOrange:Bool = true
+let turnipsAreDelicious = false
 
 
