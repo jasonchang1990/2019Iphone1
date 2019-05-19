bea6ff6c6d74d4f4ac0830501952fac6c6eb3ccf
徐國堂 <roberthus2003@gmail.com>
2019-05-19 12:29:39 +0800

    modify

---
+++ 上課同步範例/swift/basicOperation.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//駝峰式的命名法
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage = ""
+print(welcomeMessage)
+
+let 你好 = "您好世界"
+print(你好)
+let 🐶 = "dog"
+print(🐶)
+
+
+
+
+
