6874769098c2240264bbfe2e3617353d8dfdc3c2
徐國堂 <roberthus2003@gmail.com>
2019-05-21 21:18:28 +0800

    modify

--- 上課專案/iphone/Lesson11/CustomCell/ViewController.swift
+++ 上課專案/iphone/Lesson11/CustomCell/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 
 class ViewController: UITableViewController {
+    var citys:[[String:Any]]!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let bundle = Bundle.main
+        let pathURL = bundle.url(forResource: "citylist", withExtension: "plist")!
+        citys = NSArray(contentsOf: pathURL) as? [[String:Any]]
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +31,16 @@ class ViewController: UITableViewController {
 extension ViewController{
     //MARK: - UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 20;
+        return citys.count;
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cityDic = citys[indexPath.row]
+        let cityName = cityDic["City"] as! String;
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
-        cell.textLabel!.text = "row:\(indexPath.row)"
-        return cell;        
+        cell.textLabel!.text = cityName
+        return cell;
     }
 }
 
