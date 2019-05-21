f94624fb7a10bb4e43c05b30e7785385ffab46e7
徐國堂 <roberthus2003@gmail.com>
2019-05-21 20:36:51 +0800

    modify

--- 上課專案/iphone/Lesson11/CustomCell/ViewController.swift
+++ 上課專案/iphone/Lesson11/CustomCell/ViewController.swift
@@ -18,7 +18,23 @@ class ViewController: UITableViewController {
         tableView.dataSource = self;
  */
     }
+}
 
+extension ViewController{
+    //MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 20;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        cell.textLabel!.text = "row:\(indexPath.row)"
+        return cell;        
+    }
+}
 
+extension ViewController{
+    //MARK: - UITableViewDelegate
+    
 }
 
