dd0a7091344e952899180dc9245ed70ef70b195f
徐國堂 <roberthus2003@gmail.com>
2019-05-26 16:32:36 +0800

    modify

--- 上課同步範例/iphone/dictionary/dictionary/ViewController.swift
+++ 上課同步範例/iphone/dictionary/dictionary/ViewController.swift
@@ -18,6 +18,39 @@ func run(){
     print(i);
 }
 
+func run1(){
+    var airports:[String:String] = [
+        "YYZ":"Toronto",
+        "DUB":"Dublin",
+    ]
+    print("airports's count:\(airports.count)")
+    if !airports.isEmpty{
+        print("不是空的")
+    }
+    airports["LHR"] = "London"
+    airports["LHR"] = "London Heathrow"
+    print(airports)
+    if let airportName = airports["DUb"] {
+        print("airportName=\(airportName)");
+    }else{
+        print("no key");
+    }
+    
+    airports["APL"] = "Apple Internatinal"
+    airports["APL"] = nil
+    
+    print(airports)
+    
+    for (airportCode, airportName) in airports{
+        print("\(airportCode):\(airportName)");
+    }
+    let keys = Array(airports.keys)
+    let values = Array(airports.values)
+    print("keys:\(keys)");
+    print("values:\(values)");
+}
+
+
 var i = 0;
 
 
@@ -25,8 +58,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        i = i + 1
-        print(i);
+        run1()
     }
 
 
