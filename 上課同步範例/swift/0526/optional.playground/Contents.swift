d243fc5e6e6e0ee1f32ac11c6802782c6fde3633
徐國堂 <roberthus2003@gmail.com>
2019-05-26 10:34:35 +0800

    Update Contents.swift

--- 上課同步範例/swift/0526/optional.playground/Contents.swift
+++ 上課同步範例/swift/0526/optional.playground/Contents.swift
@@ -20,5 +20,44 @@ if let actualNumber = Int(possibleNumber){
     print("無法轉換");
 }
 
+if let firstNumber = Int("4"){
+    if let secondNumber = Int("42"){
+        if firstNumber < secondNumber && secondNumber < 100{
+            print("\(firstNumber) < \(secondNumber) < 100");
+        }
+    }
+}
+
+if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100{
+    print("\(firstNumber) < \(secondNumber) < 100");
+}
+
+let possibleString:String? = "An optional string."
+let forcedString:String = possibleString!
+
+let assumedString:String! = "An implicitly unwrapped optional stirng."
+let implicitStirng:String = assumedString;
+
+if assumedString != nil {
+    print(assumedString!);
+}
+
+if let definiteString = assumedString{
+    print(definiteString)
+}
+
+func canThrowAnError() throws{
+    
+}
+
+do{
+    try canThrowAnError()
+}catch{
+    //有錯誤出現就請執行這裏
+}
+
+let age = -3
+assert(age >= 0,"A person's age can't be less than zero")
+
 
 
