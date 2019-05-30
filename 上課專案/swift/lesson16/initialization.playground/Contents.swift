9f6e4d8731754fbfe1af3a1f26dd488a4c58a8d8
徐國堂 <roberthus2003@gmail.com>
2019-05-30 20:16:54 +0800

    modify

--- 上課專案/swift/lesson16/initialization.playground/Contents.swift
+++ 上課專案/swift/lesson16/initialization.playground/Contents.swift
@@ -62,5 +62,71 @@ let halfGraf = Color(white: 0.5)
 //let veryGreen = Color(0.0, 1.0, 0.0)
 let bodyTemperature = Celsius(37.0)
 
+class SurveyQuestion{
+    let text:String
+    var response:String?
+    init(text:String){
+        self.text = text;
+    }
+    
+    func ask(){
+        print(text)
+    }
+}
+
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.response = "Yes, I do like cheese."
+
+//default Initializers
+class ShoppingListItem{
+    var name:String?
+    var quantity = 1
+    var purchased = false
+}
+
+var item = ShoppingListItem();
+
+
+
+//struct
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+//memberwise init
+let twoByTwo = Size(width: 2.0, height: 2.0)
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point();
+    var size = Size();
+    init(){
+    }
+    
+    init(origin:Point, size:Size){
+        self.origin = origin
+        self.size = size
+    }
+    
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width/2)
+        let originY = center.y - (size.height/2)
+        self.init(origin: Point.init(x: originX, y: originY), size: size)
+    }
+}
+
+//let basicRect = Rect(origin: Point.init(x: 2.0, y: 2.0), size: Size.init(width: 5.0, height: 5.0))
+
+let originRect = Rect(center: Point.init(x: 2.0, y: 2.0), size: Size.init(width: 5.0, height: 5.0))
+originRect.origin.x
+originRect.origin.y
+originRect.size.width
+originRect.size.height
+
+
+
 
 
