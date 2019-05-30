dbbbc796fd1cc4199c67870d6722a7124284e9ee
徐國堂 <roberthus2003@gmail.com>
2019-05-30 21:30:25 +0800

    modify

---
+++ 上課專案/iphone/Lesson14/CustomCell/ViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  CustomCell
+//
+//  Created by Robert on 2019/5/16.
+//  Copyright © 2019 ML. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UITableViewController {
+    var citys:[[String:Any]]!
+    lazy var cityIsMarked = Array(repeating: false, count: citys.count)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let bundle = Bundle.main
+        let pathURL = bundle.url(forResource: "citylist", withExtension: "plist")!
+        citys = NSArray(contentsOf: pathURL) as? [[String:Any]]
+        //cityIsMarked = Array(repeating: false, count: citys.count)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //1-------
+        navigationItem.title = "每一個城市";
+        navigationItem.rightBarButtonItem = editButtonItem;
+        //end1--------
+    }
+}
+
+extension ViewController{
+    //MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return citys.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cityDic = citys[indexPath.row]
+        let cityName = cityDic["City"] as! String;
+        let country = cityDic["Country"] as! String;
+        let continent = cityDic["Continent"] as! String;
+        let imageName = cityDic["Image"] as! String;
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! CustomCell
+        cell.cityNameLabel.text = cityName
+        cell.countryNameLabel.text = country
+        cell.continentNameLabel.text = continent
+        cell.cityImageView.image = UIImage(named: imageName)
+        return cell;
+    }
+    //30_1
+    /*
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        
+        if editingStyle == .delete {
+            let deleteIndex = indexPath.row
+            citys.remove(at: deleteIndex)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+           
+            
+        }
+        
+    }
+ */
+    
+}
+
+extension ViewController{
+    //MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let city = citys[indexPath.row]
+        //3
+        navigationItem.title = city["City"] as? String
+        //3end
+        let cityName = city["City"] as! String
+        let optionMenu = UIAlertController(
+            title: "警告",
+            message: "您選擇的城市是\(cityName)\n您想要做什麼",
+            preferredStyle: .actionSheet)
+        
+        let alertAction_ok = UIAlertAction(
+            title: "標示",
+            style: .default){
+                (alertAction:UIAlertAction) in
+                var isMarket = self.cityIsMarked[indexPath.row]
+                isMarket = !isMarket;
+                self.cityIsMarked[indexPath.row] = isMarket;
+                guard let cell = tableView.cellForRow(at: indexPath) else{
+                    return;
+                }
+                cell.accessoryType = isMarket ? .checkmark : .none
+        }
+        
+        let alertAction_contact = UIAlertAction(
+            title: "聯絡旅行社",
+            style: .default){
+                (alertAction:UIAlertAction) -> Void in
+                let busyController = UIAlertController(
+                    title: "警告",
+                    message: "忙線中",
+                    preferredStyle: .alert)
+                let okAction = UIAlertAction(
+                    title: "OK",
+                    style: .default,
+                    handler: nil)
+                busyController.addAction(okAction)
+                self.present(busyController, animated: true, completion: nil)
+        }
+        
+        let alertAction_cancel = UIAlertAction(
+            title: "取消",
+            style: .cancel, handler:
+            nil)
+        
+        optionMenu.addAction(alertAction_ok)
+        optionMenu.addAction(alertAction_contact)
+        optionMenu.addAction(alertAction_cancel)
+        present(optionMenu, animated: true){
+            print("展示完成");
+        }
+        
+        
+        
+    }
+    //30_1
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?{
+        let deleteAction = UITableViewRowAction(
+            style: .default,
+            title: "刪除"
+        ){
+            (action:UITableViewRowAction,indexPath:IndexPath) -> Void in
+            let deleteIndex = indexPath.row
+            self.citys.remove(at: deleteIndex)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            self.cityIsMarked.remove(at: deleteIndex)
+        }
+        
+        let shareAction = UITableViewRowAction(
+            style: .default,
+            title: "分享") { (action:UITableViewRowAction, indexPath:IndexPath) in
+                let defaultText = "請直接打電話給" + (self.citys[indexPath.row]["City"] as! String);
+                let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+                self.present(activityController, animated: true, completion: nil)
+        }
+        let r = 48.0;
+        
+        let b = 99.0
+        shareAction.backgroundColor = UIColor(red: CGFloat(r)/255.0, green: 173.0/255.0, blue: CGFloat(b) / 255.0, alpha: 1.0)
+        
+        return [deleteAction,shareAction]
+    }
+}
+
