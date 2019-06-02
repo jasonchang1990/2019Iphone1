f329b797f5356d8ee47d76debfc45f552899f864
徐國堂 <roberthus2003@gmail.com>
2019-06-02 15:49:57 +0800

    modify

--- 上課同步範例/iphone/bmi/bmi/ViewController.swift
+++ 上課同步範例/iphone/bmi/bmi/ViewController.swift
@@ -23,19 +23,44 @@ class ViewController: UIViewController {
         var bmi = 0.0;
         if let heightValue = Double(heightString), let weightValue = Double(weightString){
             switch (heightValue, weightValue){
-            case (100...250,40...150):
-                bmi = weightValue / ((heightValue/100) * (heightValue/100));
-                print("標準數值bmi = \(bmi)");
-            default:
-                print("數值有誤");
+                case (100...250,40...150):
+                    bmi = weightValue / ((heightValue/100) * (heightValue/100));
+                
+                default:
+                    
+                    navigationItem.prompt =  "數值有誤"
+                    return;
             }
-            
+           
             
             
             
         }else{
-            print("輸入內容格式有誤");
+            
+            navigationItem.prompt =  "輸入內容格式有誤"
+            return;
         }
+        
+        var message = "";
+        switch bmi{
+            case 9.0..<18.5:
+                message = "過輕";
+            case 18.5..<24.0:
+                message = "剛好"
+            case 24.0..<27.0:
+                message = "過重"
+            case 27.0..<30.0:
+               
+                message = "輕度肥胖"
+            case 30.0..<35.0:
+                message = "中度肥胖"
+            case let x where x > 35.0:
+                message = "重度肥胖"
+            default:break;
+        }
+        
+        navigationItem.prompt = message;
+        
         
     }
 
