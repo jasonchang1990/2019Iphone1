fe91e87b66e4696a8bd28513518f60a46f001ac3
徐國堂 <roberthus2003@gmail.com>
2019-06-02 09:54:10 +0800

    Update Contents.swift

--- 上課同步範例/swift/0602/controlFlow.playground/Contents.swift
+++ 上課同步範例/swift/0602/controlFlow.playground/Contents.swift
@@ -4,3 +4,39 @@ let names = ["Anna", "Alex", "Brian", "Jack"]
 for name in names{
     print("Hello, \(name)");
 }
+
+let numberOfLegs = ["spider": 8, "ant": 6, "cat":4]
+for (animalName, legCount) in numberOfLegs{
+    print("animalName:\(animalName)");
+    print("legCount:\(legCount)");
+}
+
+for index in 1...5{
+    print("\(index) times 5 is \(index * 5)");
+}
+
+let base = 3
+let power = 10
+var answer = 1
+
+for _ in 1...power{
+    answer *= base;
+}
+
+print("\(answer)");
+
+let minutes = 60;
+for tickMark in 0..<minutes{
+    print(tickMark)
+}
+
+let minuteInterval = 5;
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval){
+    print(tickMark);
+}
+
+for tickMark in stride(from: 0, through: minutes, by: minuteInterval){
+    print(tickMark);
+}
+
+
