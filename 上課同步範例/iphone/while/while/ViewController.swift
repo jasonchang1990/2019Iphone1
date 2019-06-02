b46757b4882ecb047f6ea16c37e0899096b58e91
徐國堂 <roberthus2003@gmail.com>
2019-06-02 10:32:11 +0800

    modify

--- 上課同步範例/iphone/while/while/ViewController.swift
+++ 上課同步範例/iphone/while/while/ViewController.swift
@@ -8,9 +8,37 @@
 
 import UIKit
 
-func run(){
-  let abc = 10;
-    print(abc);
+func runWhile(){
+    let finalSquare = 25;
+    var board = [Int](repeating: 0, count: finalSquare + 1)
+    board[03] = +08;
+    board[06] = +11;
+    board[09] = +09;
+    board[10] = +02;
+    board[14] = -10;
+    board[19] = -11;
+    board[22] = -02;
+    board[24] = -08;
+    
+    var square = 0;
+    var diceRoll = 0;
+    var i = 0;
+    while square < finalSquare{
+        i += 1;
+        diceRoll += 2;
+        if diceRoll >= 7 {
+            diceRoll = 1;
+        }
+        
+        square += diceRoll;
+        
+        if square < board.count{
+            square += board[square];
+        }
+    }
+    
+    print("game over!")
+    print("執行的次數\(i)");
 }
 
 
@@ -18,7 +46,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        run();
+        runWhile();
     }
 
 
