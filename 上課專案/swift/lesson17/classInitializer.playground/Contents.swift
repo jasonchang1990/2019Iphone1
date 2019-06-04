b2b0db749d37cd5cce8ecc9e50cefcebf314d90b
徐國堂 <roberthus2003@gmail.com>
2019-06-04 19:46:08 +0800

    modify

---
+++ 上課專案/swift/lesson17/classInitializer.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class Vehicle{
+    var numberOfWheels = 0
+    var description:String{
+        return "\(numberOfWheels) wheel(s)"
+    }
+}
+
+let vehicle = Vehicle();
+print(vehicle.description);
+
+class Bicycle:Vehicle{
+    override init(){
+        super.init();
+        numberOfWheels = 2;
+    }
+}
+
+let bicycle = Bicycle();
+print("Bicycle:\(bicycle.description)");
+
+
+class Hoverboard:Vehicle{
+    var color:String;
+    init(color:String){
+        self.color = color;
+        super.init();
+    }
+    
+    override var description:String{
+        return "\(super.description) in a beautiful \(color)"
+    }
+}
+
+
+let hoverboard = Hoverboard(color: "silver")
+print("Hoverboard:\(hoverboard.description)");
