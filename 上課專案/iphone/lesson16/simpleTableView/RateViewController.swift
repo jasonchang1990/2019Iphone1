7875061fa70b46f5388b6e06df5af7479c1588ba
徐國堂 <roberthus2003@gmail.com>
2019-06-13 20:59:34 +0800

    modify

---
+++ 上課專案/iphone/lesson16/simpleTableView/RateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RateViewController.swift
+//  Animation
+//
+//  Created by Robert on 2019/6/13.
+//  Copyright © 2019 gjun. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+    @IBOutlet var backgroundImageView:UIImageView!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blurEffect = UIBlurEffect(style: .dark);
+        let blurEffectView = UIVisualEffectView(effect: blurEffect);
+        blurEffectView.frame = view.bounds;
+        backgroundImageView.addSubview(blurEffectView);
+    }
+
+    
+
+}
