0e3ce24c437e60772de8703d5056b14b889878e5
徐國堂 <roberthus2003@gmail.com>
2019-06-20 19:32:21 +0800

    modify

---
+++ 上課專案/swift/lesson22/protocol.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+
+/*
+struct SomeStructure:FirstProtocol,AnotherProtocol{
+    
+}
+ */
+
+//property Requirements
+
+protocol SomeProtocol{
+    var mustBeSettable:Int {get set}
+    var doesNotNeedToBeSettable:Int {get}
+}
+
+protocol AnotherProtocol{
+    static var someTypeProperty:Int{get set}
+}
+
+protocol FullyNamed{
+    var fullName:String {get}
+}
+
+struct Person:FullyNamed{
+    var fullName:String;
+}
+
+let john = Person(fullName: "Appleseed")
+
+class Starship:FullyNamed{
+    var prefix:String?
+    var name:String
+    init(name:String, prefix:String? = nil){
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    var fullName:String{
+        return (prefix != nil ? prefix! + " " : "") + name;
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
+
