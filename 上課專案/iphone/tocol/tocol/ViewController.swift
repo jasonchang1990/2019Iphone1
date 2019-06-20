9e924f8cbffa0bb3b84de63beb446154548aadae
徐國堂 <roberthus2003@gmail.com>
2019-06-20 20:13:13 +0800

    modify

---
+++ 上課專案/iphone/tocol/tocol/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  tocol
+//
+//  Created by Robert on 2019/6/20.
+//  Copyright © 2019 ios1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var counterLabel:UILabel!;
+    var counter = 0 {
+        didSet{
+            if counter < 0 {
+                counterLabel.text = "0"
+            }else{
+                counterLabel.text = String(counter)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        counter = 10
+    }
+
+
+}
+
