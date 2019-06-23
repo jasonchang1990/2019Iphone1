edb41a2641d0d70cd4c2ca3f9d5b936868ab7e35
徐國堂 <roberthus2003@gmail.com>
2019-06-23 10:23:56 +0800

    Update Contents.swift

--- 上課同步範例/swift/0623/function.playground/Contents.swift
+++ 上課同步範例/swift/0623/function.playground/Contents.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-func mixMax(array:[Int]) -> (min:Int, max:Int){
+func mixMax(array:[Int]) -> (min:Int, max:Int)?{
+    if array.isEmpty { return nil}
     var currentMin = array[0]
     var currentMax = array[0]
     for value in array[1...]{
@@ -14,4 +15,62 @@ func mixMax(array:[Int]) -> (min:Int, max:Int){
     return (currentMin, currentMax)
 }
 
-let bounds = mixMax(array: [8, -6, 2, 109, 3, 71])
+if let bounds = mixMax(array: [8, -6, 2, 109, 3, 71]){
+    //optional binding
+    print(bounds.max);
+    print(bounds.min);
+}
+
+if let num = Int("123a"){
+    print(num);
+}
+//定義一個function
+func someFunction(firstParameter:Int, secondParameterName:Int){
+    //參數名稱
+}
+
+func someFunction(first firstParameter:Int, secondParameterName:Int){
+    //參數名稱
+}
+
+//呼叫
+someFunction(firstParameter: 3, secondParameterName: 2)
+
+func someFunction(argumentLabel parameterName:Int){
+    
+}
+
+func greet(person:String, from hometown:String) -> String{
+    return "您好\(person)! 很高你的參觀\(hometown)"
+}
+
+greet(person: "robert", from: "Taipei")
+
+//overload
+//1.不同的參數數量
+//2.參數的資料類型不一樣
+//3.不同的引數標籤名稱
+
+//省略引數名稱
+func someFunction(_ firstParameterName:Int,secondParameterName:Int){
+    
+}
+
+someFunction(3, secondParameterName: 5)
+
+//Default 參數名稱
+func someFunction(parameterWithoutDefault:Int, parameterWithDefault:Int = 12){
+
+}
+someFunction(parameterWithoutDefault: 3)
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 5)
+
+func arithmeticMean(_ numbers:Double...) -> Double{
+    var total:Double = 0;
+    for number in numbers{
+        total += number;
+    }
+    return total/Double(numbers.count)
+}
+
+print(arithmeticMean(3.5, 67.4, 98.2, 35.7))
