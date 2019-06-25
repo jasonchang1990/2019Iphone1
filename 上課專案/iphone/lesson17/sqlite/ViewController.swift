bb654c47466e835c8925dded43cf9185a8006a03
徐國堂 <roberthus2003@gmail.com>
2019-06-25 20:34:05 +0800

    modify

--- 上課專案/iphone/lesson17/sqlite/ViewController.swift
+++ 上課專案/iphone/lesson17/sqlite/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     override func awakeFromNib() {
-        super.awakeFromNib();
+        super.awakeFromNib()
+       let dbSourcePath = Bundle.main.path(forResource: "city", ofType: "db")!
        let targetPaths = NSSearchPathForDirectoriesInDomains(
             .documentDirectory,
             .userDomainMask,
             true);
-        let targetpath = targetPaths.first!;
-        print(targetpath)
+        let targetpath = targetPaths.first!
+        //print(targetpath)
+        let dbTargetPath = "\(targetpath)/citys.db"
+        if !FileManager.default.fileExists(atPath: dbTargetPath){
+            if (try? FileManager.default.copyItem(atPath: dbSourcePath, toPath: dbTargetPath)) != nil {
+               print("copy成功")
+            }else{
+               print("copy失敗")
+            }
+            print("dbTargetPath路徑\(targetpath)");
+        }
     }
 
     override func viewDidLoad() {
