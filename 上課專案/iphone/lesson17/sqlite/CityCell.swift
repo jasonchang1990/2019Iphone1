c331ea3302b4813dc9484a263c3ccacdcff48959
徐國堂 <roberthus2003@gmail.com>
2019-06-27 21:28:44 +0800

    modify

---
+++ 上課專案/iphone/lesson17/sqlite/CityCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityCell.swift
+//  sqlite
+//
+//  Created by Robert on 2019/6/27.
+//  Copyright © 2019 ios1. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
