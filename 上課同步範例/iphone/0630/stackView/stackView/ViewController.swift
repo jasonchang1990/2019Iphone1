d0e26b3faae05f0a8a2dcc61051e1c03e7947d4c
徐國堂 <roberthus2003@gmail.com>
2019-06-30 11:09:24 +0800

    modify

---
+++ 上課同步範例/iphone/0630/stackView/stackView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  stackView
+//
+//  Created by Robert on 2019/6/30.
+//  Copyright © 2019 ios1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myTitle:UILabel!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTitle.text = "StackView Show";
+    }
+
+
+}
+
