f563a1347664e78965d8e0a446eb1ebce49b4a2c
徐國堂 <roberthus2003@gmail.com>
2019-06-30 14:57:14 +0800

    Update Contents.swift

--- 上課同步範例/swift/0630/structureAndClass.playground/Contents.swift
+++ 上課同步範例/swift/0630/structureAndClass.playground/Contents.swift
@@ -25,5 +25,40 @@ print(someVideoMode.resolution.width);
 //結構類型有智慧型初始化
 let vga = Resolution(width: 640, height: 480)
 
-
+//enum,structure(Int,Float,Double,String,Array,Dictionary,Bool)
 //什麼叫value type
+let hd = Resolution(width: 1920, height: 1080);
+var cinema = hd;
+cinema.width = 2048;
+print(cinema.width);
+print(hd.width)
+
+
+enum CompassPoint{
+    case north, south, east, west;
+    mutating func turnNorth(){
+        self = .north;
+    }
+}
+
+var currentDirection = CompassPoint.west
+
+let rememberedDirection = currentDirection
+
+currentDirection.turnNorth();
+
+rememberedDirection
+currentDirection
+
+//class和closure
+//什麼是reference type
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty;
+alsoTenEighty.frameRate = 30.0
+tenEighty.frameRate;
