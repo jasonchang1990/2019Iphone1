6fb12eb389866abac744e76907e30cf53d5f2b04
徐國堂 <roberthus2003@gmail.com>
2019-06-30 11:50:58 +0800

    modify

--- 上課同步範例/iphone/0630/stackView/stackView/ViewController.swift
+++ 上課同步範例/iphone/0630/stackView/stackView/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var myTitle:UILabel!;
+    @IBOutlet var myImageView:UIImageView!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myTitle.text = "StackView Show";
+        myImageView.layer.cornerRadius = 20;
+        myImageView.layer.borderWidth = 2;
+        myImageView.layer.borderColor = UIColor.darkGray.cgColor;
+        myImageView.layer.shadowColor = UIColor.red.cgColor;
+        myImageView.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        myImageView.layer.shadowOpacity = 0.7
+        myImageView.layer.shadowRadius = 5
     }
 
 
