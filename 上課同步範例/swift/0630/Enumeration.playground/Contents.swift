d65b160b2546f747d1910b7b1977788b73fa4a60
徐國堂 <roberthus2003@gmail.com>
2019-06-30 10:24:02 +0800

    Update Contents.swift

--- 上課同步範例/swift/0630/Enumeration.playground/Contents.swift
+++ 上課同步範例/swift/0630/Enumeration.playground/Contents.swift
@@ -45,3 +45,20 @@ let numberOfChoices = Beverage.allCases.count
 for beverage in Beverage.allCases{
     print(beverage)
 }
+
+
+//associate value
+enum Barcode{
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+//productBarcode = Barcode.qrCode("ABCDEFGHIJKLMNOP")
+
+switch productBarcode{
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC:\(numberSystem),\(manufacturer), \(product),\(check)");
+case .qrCode(let productCode):
+    print("QR Code:\(productCode)");
+}
