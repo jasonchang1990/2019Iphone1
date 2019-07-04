725847fca570a3eaddb03e5227871e0b55528616
徐國堂 <roberthus2003@gmail.com>
2019-07-04 21:09:01 +0800

    modify

--- 上課專案/iphone/lesson17/sqlite/DetailViewController.swift
+++ 上課專案/iphone/lesson17/sqlite/DetailViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class DetailViewController: UITableViewController {
     var city:City!;
     @IBOutlet var cityImageView:UIImageView!;
+    @IBOutlet var cityLabel:UILabel!
+    @IBOutlet var continentLabel:UILabel!
+    @IBOutlet var countryLabel:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "\(city.country ?? "沒國家") - \(city.city ?? "沒城市")";
+        title = "\(city.country ?? "沒國家") - \(city.city ?? "沒城市")"
         cityImageView.image = UIImage(named: city.image)
+        cityLabel.text = "城市:\(city.city ?? "")"
+        continentLabel.text = "洲:\(city.continent ?? "")"
+        countryLabel.text = "國家:\(city.country ?? "")"
     }
 
     
