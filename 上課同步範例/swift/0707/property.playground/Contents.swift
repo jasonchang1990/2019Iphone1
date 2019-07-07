09ae9b61673065df8eb3f76b444289ea69e21637
徐國堂 <roberthus2003@gmail.com>
2019-07-07 10:19:10 +0800

    Update Contents.swift

--- 上課同步範例/swift/0707/property.playground/Contents.swift
+++ 上課同步範例/swift/0707/property.playground/Contents.swift
@@ -15,3 +15,19 @@ let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 6;
 print(rangeOfFourItems.firstValue)
 print(rangeOfFourItems.length)
+
+//lazy stored property
+class DataImporter{
+    var filename = "data.txt"
+}
+
+class DataManager{
+    lazy var importer = DataImporter();
+    var data = [String]();
+}
+
+let manager = DataManager();
+manager.data.append("some data")
+manager.data.append("some more data")
+print(manager.importer.filename);
+
