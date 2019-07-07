2c1e5384006eab4e4d67669f4f8f17237a7f21e9
徐國堂 <roberthus2003@gmail.com>
2019-07-07 14:42:05 +0800

    modify

--- 上課同步範例/iphone/citys07_07/citys07_07/DataSource.swift
+++ 上課同步範例/iphone/citys07_07/citys07_07/DataSource.swift
@@ -8,12 +8,28 @@
 
 import Foundation
 class DataSource{
-    
-    lazy var citys:[[String:Any]] = {
+    //type store property
+    static var citys:[[String:Any]] = {
         //default value使用closure的執行，就可以有多行程式
         //只會被執行一次;
         //1.Bundle取得絕對路徑
         let path = Bundle.main.url(forResource: "citylist", withExtension: "plist")!
         return  NSArray(contentsOf: path) as! [[String:Any]]
     }()
+    
+    private init(){
+        
+    }
+    
+    
+    static var names:[String]{
+        //type computed property
+        //呼叫一次就執行一次
+        var names = [String]();
+        for cityDic in citys{
+            let cityName = cityDic["City"] as! String
+            names.append(cityName)
+        }
+        return names;
+    }
 }
--- 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
+++ 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var dataSource = DataSource();
-    
+class ViewController: UIViewController {    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("city的數量\(dataSource.citys.count)");
+        print("city的數量\(DataSource.citys.count)");
+        print("city的城市名:\(DataSource.names)");
     }
 
 
