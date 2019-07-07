c6ce50a45cf66fc9475db53dfb484396901a9a76
徐國堂 <roberthus2003@gmail.com>
2019-07-07 10:43:20 +0800

    Update Contents.swift

--- 上課同步範例/swift/0707/property.playground/Contents.swift
+++ 上課同步範例/swift/0707/property.playground/Contents.swift
@@ -31,3 +31,42 @@ manager.data.append("some data")
 manager.data.append("some more data")
 print(manager.importer.filename);
 
+
+//computed property
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+struct Rect{
+    var origin = Point();
+    var size = Size();
+    //computed propert 語法
+    var center:Point{
+        get{
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        
+        set{
+            //newValue
+            origin.x = newValue.x - (size.width / 2);
+            origin.y = newValue.y - (size.height / 2);
+        }
+    }
+}
+
+var square = Rect(origin: Point.init(x: 0.0, y: 0.0), size: Size.init(width: 10.0, height: 10.0));
+let initialSquareCenter = square.center;
+print(initialSquareCenter.x)
+print(initialSquareCenter.y)
+
+square.center = Point(x: 15.0, y: 15.0)
+print(square.origin.x);
+print(square.origin.y);
+
+
