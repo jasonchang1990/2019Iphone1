d8171bb6e69b6ae4f906238bb5e30ebddedf972b
徐國堂 <roberthus2003@gmail.com>
2019-07-07 14:59:02 +0800

    modify

--- 上課同步範例/iphone/citys07_07/citys07_07/DataSource.swift
+++ 上課同步範例/iphone/citys07_07/citys07_07/DataSource.swift
@@ -21,7 +21,7 @@ class DataSource{
         
     }
     
-    
+    /*
     static var names:[String]{
         //type computed property
         //呼叫一次就執行一次
@@ -32,4 +32,38 @@ class DataSource{
         }
         return names;
     }
+ */
+    
+    static var names:[String] = {
+        //type property,使用default value 的closure
+        //只會執行一次
+        var names = [String]();
+        for cityDic in citys{
+            let cityName = cityDic["City"] as! String
+            names.append(cityName)
+        }
+        return names;
+    }()
+    
+    static var continents:[String] = {
+        //type property,使用default value 的closure
+        //只會執行一次
+        var continentNames = Set<String>();
+        for cityDic in citys{
+            let continentName = cityDic["Continent"] as! String
+            continentNames.insert(continentName)
+        }
+        return continentNames.sorted()
+    }()
+    
+    static var countries:[String] = {
+        //type property,使用default value 的closure
+        //只會執行一次
+        var countryNames = Set<String>();
+        for cityDic in citys{
+            let countryName = cityDic["Country"] as! String
+            countryNames.insert(countryName)
+        }
+        return countryNames.sorted()
+    }()
 }
--- 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
+++ 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("city的數量\(DataSource.citys.count)");
-        print("city的城市名:\(DataSource.names)");
+        //print("city的數量\(DataSource.citys.count)");
+        //print("city的城市名:\(DataSource.names)");
+        //print("continent的洲名:\(DataSource.continents)");
+        print("country名稱:\(DataSource.countries)");
     }
 
 
