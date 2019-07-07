d6cd0b5f7030e5d7b604d47bc4f135cf88a4c099
徐國堂 <roberthus2003@gmail.com>
2019-07-07 15:46:13 +0800

    modify

--- 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
+++ 上課同步範例/iphone/citys07_07/citys07_07/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var cityList:UITextView!;
+    @IBOutlet var countriesList:UITextView!;
+    @IBOutlet var continentList:UITextView!;
+    
     var namesString = "";
+    var countriesString = "";
+    var continentString = "";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //for CityName
         for cityName in DataSource.names{
             namesString += "\(cityName)\n\n"
+           
         }
         cityList.text = namesString;
-       
+        
+        //for CountryName
+        for countryName in DataSource.countries{
+           countriesString += "\(countryName) \n\n"
+        }
+        
+        countriesList.text = countriesString;
+        
+        //for ContinentName
+        for continent in DataSource.continents{
+            continentString += "\(continent) \n\n";
+        }
+        continentList.text = continentString;
     }
 
 
